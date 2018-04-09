12cb42097dd3fb303ee781661445d8b68e63f5c0
Aye10032 <yeyu6_505@126.com>
2018-04-10 01:18:34 +0800

    测试写入excel表格数据成功

--- src/test.java
+++ src/test.java
@@ -1,5 +1,13 @@
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 import net.sourceforge.tess4j.*;
+import org.apache.poi.hssf.usermodel.HSSFCell;
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 
 public class test{
 
@@ -8,12 +16,26 @@ public class test{
         File imageFile = new File("D:\\test\\1s.jpg");
         ITesseract instance = new Tesseract();
         instance.setLanguage("chi_sim");
+        String filepath = "D:\\test\\test.xls";
 
         try {
             String result = instance.doOCR(imageFile);
             System.out.print(result);
+
+            HSSFWorkbook workbook = new HSSFWorkbook();
+            HSSFSheet sheet = workbook.createSheet("test");
+            FileOutputStream out =new FileOutputStream(filepath);
+            HSSFRow row = sheet.createRow(0);
+            HSSFCell cell = row.createCell(0);
+            cell.setCellValue(result);
+            workbook.write(out);
+            out.close();
         }catch (TesseractException e){
             System.err.print(e.getMessage());
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
 
     }
