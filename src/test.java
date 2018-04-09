5f1c4e38ac259ee6144e2f9900c27639f14f54fd
Aye10032 <yeyu6_505@126.com>
2018-04-10 05:50:55 +0800

    添加循环遍历文件夹，项目大体结束

--- src/test.java
+++ src/test.java
@@ -1,6 +1,7 @@
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
@@ -18,7 +19,6 @@ import javax.imageio.ImageIO;
 
 public class test{
 
-    static String imageFile ="C:\\Users\\Aye10032\\Downloads\\1_20180208150251_x4hzz\\16";
 
     public static void main(String[] args){
 
@@ -36,39 +36,60 @@ public class test{
         HSSFCell nametop = osisjdjs.createCell(1);
         nametop.setCellValue("企业名称");
 
+        for (int i=1; i<15; i++) {
 
-        try {
+            String imageFile = "C:\\Users\\Aye10032\\Downloads\\1_20180208150251_x4hzz\\" + i;
 
-            firstDo();
+            try {
+                BufferedImage bufferedImage;
 
-            System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
-            Mat src = Imgcodecs.imread(imageFile+".jpg",Imgcodecs.CV_LOAD_IMAGE_GRAYSCALE);
+                try {
 
-            if (src.empty()){
-                throw new Exception("no file");
-            }
+                    bufferedImage = ImageIO.read(new File(imageFile + ".png"));
 
-            Mat dst = new Mat();
-            Imgproc.threshold(src,dst,110.0,225.0,Imgproc.THRESH_BINARY);
-            Imgcodecs.imwrite("D:\\test\\result.jpg",dst);
+                    BufferedImage newBufferedImage = new BufferedImage(bufferedImage.getWidth(),
+                            bufferedImage.getHeight(), BufferedImage.TYPE_INT_RGB);
 
-            Rectangle rect = new Rectangle(600,80);
-            String result = instance.doOCR(mat2BI(dst),rect);
+                    newBufferedImage.createGraphics().drawImage(bufferedImage, 0, 0, Color.WHITE, null);
 
-            String str1 = result.replace(" 二 ",":");
-            String str2 = str1.replace('…','5');
+                    ImageIO.write(newBufferedImage, "jpg", new File(imageFile + ".jpg"));
 
-            System.out.print(str2);
+                    System.out.println("Done");
 
-            String num,name;
+                } catch (IOException e) {
 
-            if (str2.contains(":")) {
-                int stnum = str2.indexOf(':');
-                int ennum = str2.indexOf('\n');
-                int stname = str2.indexOf(':', ennum);
+                    e.printStackTrace();
 
-                num = str2.substring(stnum + 1, ennum);
-                name = str2.substring(stname + 1);
+                }
+
+                System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+                Mat src = Imgcodecs.imread(imageFile + ".jpg", Imgcodecs.CV_LOAD_IMAGE_GRAYSCALE);
+
+                if (src.empty()) {
+                    throw new Exception("no file");
+                }
+
+                Mat dst = new Mat();
+                Imgproc.threshold(src, dst, 110.0, 225.0, Imgproc.THRESH_BINARY);
+                Imgcodecs.imwrite("D:\\test\\result.jpg", dst);
+
+                Rectangle rect = new Rectangle(600, 80);
+                String result = instance.doOCR(mat2BI(dst), rect);
+
+                String str1 = result.replace(" 二 ", ":");
+                String str2 = str1.replace('…', '5');
+
+                System.out.print(str2);
+
+                String num, name;
+
+                if (str2.contains(":")) {
+                    int stnum = str2.indexOf(':');
+                    int ennum = str2.indexOf('\n');
+                    int stname = str2.indexOf(':', ennum);
+
+                    num = str2.substring(stnum + 1, ennum);
+                    name = str2.substring(stname + 1);
 
             /*
             System.out.println(stnum);
@@ -76,31 +97,31 @@ public class test{
             System.out.print(stname);
             */
 
-                System.out.println(num);
-                System.out.println(name);
-            }else {
-                num = "error! picture wrong";
-                name = "error! picture wrong";
+                    System.out.println(num);
+                    System.out.println(name);
+                } else {
+                    num = "error! picture wrong";
+                    name = "error! picture wrong";
+                }
+
+                FileOutputStream out = new FileOutputStream(filepath);
+                HSSFRow row = sheet.createRow(i);
+                HSSFCell NUM = row.createCell(0);
+                NUM.setCellValue(num);
+                HSSFCell NAME = row.createCell(1);
+                NAME.setCellValue(name);
+                workbook.write(out);
+                out.close();
+
+            } catch (TesseractException e) {
+                System.err.print(e.getMessage());
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (Exception e) {
+                e.printStackTrace();
             }
-
-            FileOutputStream out =new FileOutputStream(filepath);
-            HSSFRow row = sheet.createRow(1);
-            HSSFCell NUM = row.createCell(0);
-            NUM.setCellValue(num);
-            HSSFCell NAME = row.createCell(1);
-            NAME.setCellValue(name);
-            workbook.write(out);
-            out.close();
-
-        }catch (TesseractException e){
-            System.err.print(e.getMessage());
-        } /*catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        */ catch (Exception e) {
-            e.printStackTrace();
         }
 
     }
@@ -123,31 +144,6 @@ public class test{
         image.getRaster().setDataElements(0,0,mat.cols(),mat.rows(),data);
 
         return image;
-    }
-
-    public static void firstDo(){
-
-        BufferedImage bufferedImage;
-
-        try {
-
-            bufferedImage = ImageIO.read(new File(imageFile+".png"));
-
-            BufferedImage newBufferedImage = new BufferedImage(bufferedImage.getWidth(),
-                    bufferedImage.getHeight(), BufferedImage.TYPE_INT_RGB);
-
-            newBufferedImage.createGraphics().drawImage(bufferedImage, 0, 0, Color.WHITE, null);
-
-            ImageIO.write(newBufferedImage, "jpg", new File(imageFile+".jpg"));
-
-            System.out.println("Done");
-
-        } catch (IOException e) {
-
-            e.printStackTrace();
-
-        }
-
     }
 
 
